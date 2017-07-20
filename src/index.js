418af543d6830c93a0bd8dfe38a59a47b8807d64
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2017-07-20 13:30:51 +0300

    Changeset to ReactJS initial repo commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+import App from './index2';
+//import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
+//ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
