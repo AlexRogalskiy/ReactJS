c1a6d9ef49edb6e5b9a0c436e19b3cc39de8a5ff
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2017-07-20 17:52:04 +0300

    Changeset to ReactJS components fix

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import App from './index2';
+//import App from './index2';
+//import App from './index3';
+import App from './index4';
 //import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 //registerServiceWorker();
-//ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'));
