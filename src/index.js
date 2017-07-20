68efdfdeddbb28c7f25d3a73a57ad2c7fc7ff961
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2017-07-21 00:58:09 +0300

    Changeset to css animation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 //import App from './App';
 //import App from './index2';
 //import App from './index3';
-import App from './index4';
+//import App from './index4';
+import App from './index5';
 //import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
