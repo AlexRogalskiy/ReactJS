c33dcb5c7decb236e72d4e1dc13e580f2349eec1
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2018-02-12 02:39:01 +0300

    Changeset to libs

--- simple/views/view8/public/libs/utils.js
+++ simple/views/view8/public/libs/utils.js
@@ -32,7 +32,7 @@ function format() {
     });
 };
 
- function format2(format) {
+function format2(format) {
      var args = Array.prototype.slice.call(arguments, 1);
      return format.replace(/{(\d+)}/g, function(match, number) { 
          return typeof args[number] != 'undefined'
@@ -40,3 +40,23 @@ function format() {
             : match;
      });
 };
+
+function humanize(size) {
+    if(size < 0) return;
+    var units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
+    var ord = Math.floor(Math.log(size) / Math.log(1024));
+    ord = Math.min( Math.max(0, ord), units.length - 1);
+    var s = Math.round((size / Math.pow(1024, ord)) * 100) / 100;
+    return s + ' ' + units[ord];
+};
+
+// $('a.fetchSize').each(function() {
+//     $.ajax({
+//         type: 'HEAD',
+//         url: link.href,
+//         complete: function(xhr) {
+//             var size = humanize(xhr.getResponseHeader('Content-Length'));
+//             $(link).append('(' + size + ')');
+//         }
+//     });
+// });
